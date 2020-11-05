943886144a88c8b1898c4d0936a77a790f886c13
enmccloud <66036830+enmccloud@users.noreply.github.com>
2020-11-05 08:55:35 -0600

    Initial Commit- Program Shell
    Shell

---
+++ petsitting/src/main/java/pet/PetsittingApplication.java
@@ -0,0 +1,13 @@
+package pet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PetsittingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PetsittingApplication.class, args);
+	}
+
+}
