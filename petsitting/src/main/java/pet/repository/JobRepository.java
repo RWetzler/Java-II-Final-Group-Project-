44508ce404e058cc598f81eb341ec8e3152963af
Hillary-Murphy <hillary.j.murphy@gmail.com>
2020-11-18 21:32:26 -0600

    Create JobRepository.java
    Creating Job repository

---
+++ petsitting/src/main/java/pet/repository/JobRepository.java
@@ -0,0 +1,11 @@
+package pet.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import pet.beans.Job;
+
+@Repository
+public interface JobRepository extends JpaRepository<Job, Long>{
+
+}
