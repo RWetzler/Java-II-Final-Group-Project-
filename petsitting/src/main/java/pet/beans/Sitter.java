cd30c16e3eadbf116d15d22053ade656ae879aac
Hillary-Murphy <hillary.j.murphy@gmail.com>
2020-11-28 21:43:24 -0600

    sitter-biography
    Adding biography to sitter. Also tweaking sitter bean.

--- petsitting/src/main/java/pet/beans/Sitter.java
+++ petsitting/src/main/java/pet/beans/Sitter.java
@@ -1,14 +1,11 @@
 package pet.beans;
 
-import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Embeddable;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.persistence.JoinColumn;
-import javax.persistence.OneToOne;
 
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -44,21 +41,13 @@ public class Sitter {
 	private String certifications;
 	@Column(name="MAX_PETS")
 	private int maxPets;
+	@Column(name="BIOGRAPHY")
+	private String biography;
 	@Column(name="AVG_RATING")
 	private double averageRating;
-	@OneToOne(cascade=CascadeType.PERSIST)
-	@JoinColumn(name="JOB_ID")
-	private Job jobId;
-	@OneToOne(cascade=CascadeType.PERSIST)
-	@JoinColumn(name="RATING")
-	private Job rating;
-	@OneToOne(cascade=CascadeType.PERSIST)
-	@JoinColumn(name="FEEDBACKTOSITTER")
-	private Job feedbackToSitter;
 
 	public Sitter(String firstName, String lastName, String userName, String passWord, String phone, String email,
-			String miscContact, String certifications, int maxPets, double averageRating, Job rating,
-			Job feedbackToSitter) {
+			String miscContact, String certifications, int maxPets, String biography, double averageRating) {
 		super();
 		this.firstName = firstName;
 		this.lastName = lastName;
@@ -69,9 +58,8 @@ public class Sitter {
 		this.miscContact = miscContact;
 		this.certifications = certifications;
 		this.maxPets = maxPets;
+		this.biography = biography;
 		this.averageRating = averageRating;
-		this.rating = rating;
-		this.feedbackToSitter = feedbackToSitter;
 	}
 
 }
